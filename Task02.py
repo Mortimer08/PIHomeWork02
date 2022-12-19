eee8bf972b4d59b365ecbdf7802882b9f65d4ad9
Ilya Makarov <mortimer08@gmail.com>
2022-12-19 15:44:36 +0500

    Task02 Complete

---
+++ Task02.py
@@ -0,0 +1,15 @@
+# Задайте список из n чисел последовательности (1 + 1/n)^n, выведеите его на экран, а так же сумму элементов списка.
+# Пример:
+# Для n=4 -> [2, 2.25, 2.37, 2.44]
+# Сумма 9.06
+
+number = int(input('Введите целое число: '))
+sum = 0
+
+sequence = []
+for i in range(1, number+1):
+    sequence.append(round((1+1/i)**i, 2))
+    sum += sequence[i-1]
+
+print(f'Для n={number} -> {sequence}')
+print(f'Сумма {sum}')
